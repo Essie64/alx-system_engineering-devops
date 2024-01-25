ff3564e64f820e1fcd88b8a82ffb69b7fa4b7bc9
Esther Kubania <122841197+dev-EstherKubania@users.noreply.github.com>
2024-01-25 23:24:16 +0300

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -16,28 +16,17 @@ def top_ten(subreddit):
     Returns:
         None
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': }
-    params = {'limit': 10}'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-
-    try:
-        response = requests.get(url, headers=headers, params=params)
-
-        if response.status_code == 200:
-            data = response.json()
-            posts = data.get('data', {}).get('children', [])
-
-            if not posts:
-                print("No posts found.")
-            else:
-                for post in posts:
-                    title = post.get('data', {}).get('title')
-                    print(title)
-
-        else:
-            print(f"Error: {response.status_code}")
-            print("None")
-
-    except Exception as e:
-        print(f"Error: {e}")
-        print("None")
+    url = 'https://www.reddit.com'
+    header = {
+        'Accept': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+    }
+    response = requests.get('{}/r/{}/.json?sort={}&limit={}'.format(
+        url, subreddit, 'top', 10),
+        headers=header,
+        allow_redirects=False)
+    if response.status_code == 200:
+        for post in response.json()['data']['children'][0:10]:
+            print(post['data']['title'])
+    else:
+        print(None)
